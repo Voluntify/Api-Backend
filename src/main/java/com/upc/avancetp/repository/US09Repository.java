b2b59db250508636ee3e1601a44ceb7207084a9c
Jairo Joseph Marin Sanchez <u202216861@upc.edu.pe>
2024-09-18 14:26:21 -0500

    Update US09Repository.java

--- src/main/java/com/upc/avancetp/repository/US09Repository.java
+++ src/main/java/com/upc/avancetp/repository/US09Repository.java
@@ -2,7 +2,7 @@ package com.upc.avancetp.repository;
 
 import com.upc.avancetp.model.Organizaciones;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface OrganizacionesRepository extends JpaRepository<Organizaciones, Long> {
