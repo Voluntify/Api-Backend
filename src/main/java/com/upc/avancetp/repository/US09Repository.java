d01b3a67b3eab93baa659b8de15bf247f57c2685
Matias Hidalgo <matias.hidalgo.2022@gmail.com>
2024-09-18 19:22:20 -0500

    Modificacion para US09

--- src/main/java/com/upc/avancetp/repository/US09Repository.java
+++
@@ -1,9 +0,0 @@
-package com.upc.avancetp.repository;
-
-import com.upc.avancetp.model.Organizaciones;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-@Repository
-public interface OrganizacionesRepository extends JpaRepository<Organizaciones, Long> {
-}
